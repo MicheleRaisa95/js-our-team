27f8b9e7a2dbd6ad02fe3ff05191728e1225345b
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-05-14 18:35:32 +0200

    Bonus 1

--- ourteam/bonus 1/assets/js/main.js
+++ ourteam/bonus 1/assets/js/main.js
@@ -8,42 +8,42 @@ const teamMembers = [
 
         ruolo: "Founder & Ceo",
 
-        foto: "wayne-barnett-founder-ceo.jpg"
+        foto: "./assets/img/wayne-barnett-founder-ceo.jpg"
     },
     {
         nome: "Angela Caroll",
 
         ruolo: "Chief Editor",
 
-        foto: "angela-caroll-chief-editor.jpg"
+        foto: "./assets/img/angela-caroll-chief-editor.jpg"
     },
     {
         nome: "Walter Gordon",
 
         ruolo: "Office Manager",
 
-        foto: "walter-gordon-office-manager.jpg"
+        foto: "./assets/img/walter-gordon-office-manager.jpg"
     },
     {
         nome: "Angela Lopez",
 
         ruolo: "Social Media Manager",
 
-        foto: "angela-lopez-social-media-manager.jpg"
+        foto: "./assets/img/angela-lopez-social-media-manager.jpg"
     },
     {
         nome: "Scott Estrada",
 
         ruolo: "Developer",
 
-        foto: "scott-estrada-developer.jpg"
+        foto: "./assets/img/scott-estrada-developer.jpg"
     },
     {
         nome: "Barbara Ramos",
 
         ruolo: "Graphic Designer",
 
-        foto: "barbara-ramos-graphic-designer.jpg"
+        foto: "./assets/img/barbara-ramos-graphic-designer.jpg"
     }
 ]
 
@@ -51,19 +51,40 @@ const teamMembers = [
 
 // for...in + console log
 
-for (let info in teamMembers) {
-    const member = teamMembers[info]
-    console.log(`Nome: ${member.nome}, Ruolo: ${member.ruolo}, Foto: ${member.foto}`);
-}
+// for (let info in teamMembers) {
+//     const member = teamMembers[info]
+//     console.log(`Nome: ${member.nome}, Ruolo: ${member.ruolo}, Foto: ${member.foto}`);
+// }
 
-// MILESTONE 2 STAMPO LE INFORMAZIONI NEL DOM
+// // MILESTONE 2 STAMPO LE INFORMAZIONI NEL DOM
 
-const  listElement = document.querySelector(".lista");
+// const  listElement = document.querySelector(".lista");
+
+// for (let info = 0; info < teamMembers.length; info++) {
+// const member = teamMembers[info];
+// const memberInfo = `Nome: ${member.nome}, Ruolo: ${member.ruolo}, Foto: ${member.foto}`;
+// const listItem = document.createElement("li");
+// listItem.innerText = memberInfo;
+// listElement.append(listItem);
+// }
+
+// -----------------------------------------------------
+
+// BONUS 1
+
+const listElement = document.querySelector(".lista");
 
 for (let info = 0; info < teamMembers.length; info++) {
-const member = teamMembers[info];
-const memberInfo = `Nome: ${member.nome}, Ruolo: ${member.ruolo}, Foto: ${member.foto}`;
-const listItem = document.createElement("li");
-listItem.innerText = memberInfo;
-listElement.append(listItem);
+    const member = teamMembers[info];
+    const memberInfo = `Nome: ${member.nome}, Ruolo: ${member.ruolo}`;
+
+    // creo elemento img
+    const imgElement = document.createElement("img");
+    imgElement.src = member.foto
+
+    const listItem = document.createElement("li");
+    listItem.innerHTML = memberInfo;
+    listItem.append(imgElement);
+
+    listElement.append(listItem)
 }
